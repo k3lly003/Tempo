e67b556735b8f6a0de396b4e9ef4520a160bf50e
k3lly003 <ntirukelly@gmail.com>
2023-08-29 13:48:37 +0300

    adding wellcome comp

---
+++ Buytopia/Wellcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+function Wellcome(props) {
+  return <View style={styles.background}></View>;
+}
+const styles = StyleSheet.create({
+  background: {
+    color: "#000",
+    flex: "1",
+  },
+});
+export default Wellcome;
